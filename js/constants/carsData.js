9d0d7a9d94c118f641b46e8aa7fd6f053745acf9
Suren <khe4oyan@gmail.com>
2025-05-23 18:26:10 +0400

    .

--- js/constants/carsData.js
+++ js/constants/carsData.js
@@ -11,6 +11,45 @@ const carsData = [
     details: "Mechanical 2.5 L / 2000 year",
     price: 25_000,
   },
+  
+  {
+    images: [
+      "car.jpg",
+      "opel_front.jpg",
+      "opel_back.jpg",
+      "opel_salon.jpg",
+    ],
+    mark: "Opel Vectra B",
+    logo: "opel.png",
+    details: "Mechanical 2.5 L / 2000 year",
+    price: 25_000,
+  },
+
+  {
+    images: [
+      "car.jpg",
+      "opel_front.jpg",
+      "opel_back.jpg",
+      "opel_salon.jpg",
+    ],
+    mark: "Opel Vectra B",
+    logo: "opel.png",
+    details: "Mechanical 2.5 L / 2000 year",
+    price: 25_000,
+  },
+
+  {
+    images: [
+      "car.jpg",
+      "opel_front.jpg",
+      "opel_back.jpg",
+      "opel_salon.jpg",
+    ],
+    mark: "Opel Vectra B",
+    logo: "opel.png",
+    details: "Mechanical 2.5 L / 2000 year",
+    price: 25_000,
+  },
 ];
 
 export default carsData;
